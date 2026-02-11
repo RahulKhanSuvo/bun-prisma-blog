bebdf2db8518309615313d69b1720d7ff0139559
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-12 02:16:55 +0600

    feat:added the first get api

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import app from "./app";
+import { prisma } from "./src/utility/db";
+const port = process.env.PORT
+async function main() {
+    try {
+        await prisma.$connect()
+        console.log("connect to database successfully")
+        app.get("/",)
+        app.listen(port, () => {
+            console.log(`server is running post on ${port}`)
+        })
+    } catch (error) {
+        console.log("an error occurred:", error)
+        await prisma.$disconnect()
+        process.exit(1)
+
+    }
+}
+main()
