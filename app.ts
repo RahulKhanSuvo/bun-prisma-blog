17a24c5946ad2580cea2c5558b92ec9770aa9260
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-12 09:35:22 +0600

    feat:added batter auth and additional field for

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import postRouter from "./src/modules/posts/post.router"
+import { auth } from "./src/lib/auth"
+import { toNodeHandler } from "better-auth/node"
 const app = express()
 app.use(express.json())
+
 app.get("/", (req, res) => {
     res.send("hello world")
 })
+app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use("/posts", postRouter)
 
 export default app
