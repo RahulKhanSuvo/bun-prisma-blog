22c53a5b5b39a98f8e5950a35a7bd084b4275b83
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-12 12:58:25 +0600

    feat:added email for validtion

--- app.ts
+++ app.ts
@@ -2,8 +2,13 @@ import express from "express"
 import postRouter from "./src/modules/posts/post.router"
 import { auth } from "./src/lib/auth"
 import { toNodeHandler } from "better-auth/node"
+import cors from "cors"
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:4000", // your frontend
+    credentials: true,               // REQUIRED for cookies
+}));
 
 app.get("/", (req, res) => {
     res.send("hello world")
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "../utility/db";
+import { transporter } from "../utility/nodeMailer";
 
 
 export const auth = betterAuth({
@@ -26,6 +27,26 @@ export const auth = betterAuth({
         }
     },
     emailAndPassword: {
-        enabled: true
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
+    },
+
+    emailVerification: {
+        sendOnSignUp: true,
+        sendVerificationEmail: async ({ user, url, token }, req) => {
+            try {
+                transporter.sendMail({
+                    from: `"My App" <${process.env.EMAIL_USER}>`,
+                    to: user.email,
+                    subject: "Hello from Bun 🚀",
+                    text: "This email is sent using Bun + Nodemailer",
+                    html: "<b>This email is sent using Bun + Nodemailer 🚀</b>",
+                })
+            } catch (error) {
+                console.log(error)
+                throw error
+            }
+        }
     }
 });     
